12d31ffebb45f6f08ad0d186001b4d4ba9ae9f1e
Ritik Gupta <ritikgupta4444@gmail.com>
2026-05-14 08:00:44 +0530

    define functions

---
+++ DSA/function_and_method9/A.php
@@ -0,0 +1,33 @@
+<?php
+
+// Define Function:
+echo "
+<pre>
+A function is:
+    - A resuse block of code
+    - used to perform a specifi task
+    - executed only when called
+
+Function Improve: 
+    - Code reusability
+    - Readability
+    - Maintainability
+
+Purpose of Function:
+    - Avoid writing the same code multiple times
+    - Organize large program
+    - Make debugger easier
+    - Improve code structure
+
+Type of Function:
+    - Built-in Function
+    - User Defined Function
+
+function sum(){
+    // write code
+}
+    sum();
+</pre>
+";
+
+?>
